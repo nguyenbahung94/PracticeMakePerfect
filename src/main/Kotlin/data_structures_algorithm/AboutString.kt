789e176dee063a7b882b2c90869b7357e44b97ae
nguyenbahung94 <mr.hungcity@gmail.com>
2024-11-23 17:52:41 +0700

    practice coding

--- src/main/Kotlin/data_structures_algorithm/AboutString.kt
+++ src/main/Kotlin/data_structures_algorithm/AboutString.kt
@@ -1,12 +1,80 @@
 package data_structures_algorithm
 
+import java.util.Stack
 import kotlin.math.max
 
 fun main() {
     // Longest substring without repeating characters
-    mainLongestSubString()
-    mainTwoSum()
-    mainBestTimeToBuyAndSell()
+  //  mainLongestSubString()
+  //  mainTwoSum()
+  //  mainBestTimeToBuyAndSell()
+  //  mainContainsDuplicate()
+  //  reverseString()
+    reverseWords()
+}
+
+fun reverseWords() {
+    val sentence = "I'm the best developer in the world!"
+    var result =""
+    var previousSpace = 0
+    var startFrom = 0
+    for (i in 0 until sentence.length) {
+        if (sentence[i] == ' ') {
+            startFrom = i - 1
+            while (startFrom >= previousSpace) {
+
+                result += sentence[startFrom]
+
+                startFrom--
+            }
+            result += " "
+            previousSpace = i +1
+        } else if (i == sentence.length -1) {
+            startFrom = i
+            while (startFrom >= previousSpace) {
+
+                result += sentence[startFrom]
+
+                startFrom--
+            }
+        }
+    }
+    println("reverseWords = $result")
+}
+
+fun reverseString() {
+    val listString = mutableListOf<String>("a", "b", "c", "d", "e", "f")
+    val stack = Stack<String>()
+    for (i in 0 until listString.size) {
+        stack.push(listString[i])
+    }
+    var i = 0
+    while (!stack.isEmpty()) {
+       listString[i] = stack.pop()
+        i++
+    }
+    println("listString = $listString")
+}
+
+fun mainContainsDuplicate() {
+    val result = containsDuplicate(arrayOf(1,2,5,7,8,9,10,11,12,2))
+    val result2 = containsDuplicate(arrayOf(1,2,5,7,8,9,10,11,12,))
+    println("contain duplicate1 = $result")
+    println("contain duplicate2 = $result2")
+}
+
+fun containsDuplicate(numbers: Array<Int>): Boolean {
+
+    val containsNumber = hashSetOf<Int>()
+
+    for (i in 0 until numbers.size) {
+        if (containsNumber.contains(numbers[i])) {
+            return true
+        } else {
+            containsNumber.add(numbers[i])
+        }
+    }
+    return false
 }
 
 fun mainBestTimeToBuyAndSell() {
